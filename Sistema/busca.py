e7f054184bd70a18f6a279dd661be50643aab0ee
BrunoSerpa <serpabruno2005@gmail.com>
2024-06-20 12:52:34 -0300

    melhorei as buscas

--- Sistema/busca.py
+++ Sistema/busca.py
@@ -1,115 +1,55 @@
-# CONEXÃO COM A COLEÇÃO DE CLIENTE
 from conexaoMongo import conectar
-from bson.objectid import ObjectId
 from formatacaoJson import usuarioJson, vendedorJson, produtoJson
-        
-def buscaUsuario(dadoProcurado, tipoDado, comCodigo):
+from bson.objectid import ObjectId
+
+def buscarDocumento(colecao, filtro, sort=None):
+    documentos = colecao.find(filtro).sort(sort) if sort else colecao.find(filtro)
+    documentos = list(documentos)
+    return documentos if documentos else None
+
+def buscarPorId(colecao, objectId): return colecao.find_one({"_id": ObjectId(objectId)})
+
+def buscarUsuario(dadoProcurado, tipoDado, comCodigo=False):
     usuarios = conectar().Usuario
     if tipoDado == 'nome':
-        if dadoProcurado == '':
-            listaUsuarios = usuarios.find().sort("nome_usuario")
-        else:
-            listaUsuarios = usuarios.find({"nome_usuario": dadoProcurado}).sort("nome_usuario")
-        count = 0
-        listaUsuarios = [*listaUsuarios]
-        if len(listaUsuarios) == 0:
+        filtro = {} if dadoProcurado == '' else {"nome_usuario": dadoProcurado}
+        listaUsuarios = buscarDocumento(usuarios, filtro, "nome_usuario")
+        if not listaUsuarios:
             print("Nenhum usuário encontrado!")
             return False
-        elif len(listaUsuarios) == 1:
-            usuario = listaUsuarios[0]
+        for usuario in listaUsuarios:
             print("===========================================")
-            if comCodigo and "_id" in usuario:
-                print(f'Id: {usuario["_id"]}') 
-            usuarioJson(usuario)
-            print("===========================================")
-            return usuario
-        else:
-            for usuario in listaUsuarios:
-                print("===========================================")
-                if comCodigo and "_id" in usuario:
-                    print(f'Id: {usuario["_id"]}') 
-                usuarioJson(usuario)
-            print("===========================================")
-            return True
-    else:
-        usuario = usuarios.find_one({"_id": ObjectId(dadoProcurado)})
-        if usuario:
-            print("Usuário encontrado!")
-            return usuario
-        else:
-            print("Nenhum Usuário encontrado!")
-            return None
+            usuarioJson(usuario, comCodigo=comCodigo)
+        print("===========================================")
+        return listaUsuarios if len(listaUsuarios) > 1 else listaUsuarios[0]
+    return buscarPorId(usuarios, dadoProcurado)
 
-def buscaVendedor(dadoProcurado, tipoDado, comCodigo):
+def buscarVendedor(dadoProcurado, tipoDado, comCodigo=False):
     vendedores = conectar().Vendedor
     if tipoDado == 'nome':
-        if dadoProcurado == '':
-            listaVendedores = vendedores.find().sort("nome_vendedor")
-        else:
-            listaVendedores = vendedores.find({"nome_vendedor": dadoProcurado}).sort("nome_vendedor")
-        count = 0
-        listaVendedores = [*listaVendedores]
-        if len(listaVendedores) == 0:
-            print("Nenhum usuário encontrado!")
+        filtro = {} if dadoProcurado == '' else {"nome_vendedor": dadoProcurado}
+        listaVendedores = buscarDocumento(vendedores, filtro, "nome_vendedor")
+        if not listaVendedores:
+            print("Nenhum vendedor encontrado!")
             return False
-        elif len(listaVendedores) == 1:
-            vendedor = listaVendedores[0]
+        for vendedor in listaVendedores:
             print("===========================================")
-            if comCodigo and "_id" in vendedor:
-                print(f'Id: {vendedor["_id"]}') 
-            vendedorJson(vendedor)
-            print("===========================================")
-            return vendedor
-        else:
-            for vendedor in listaVendedores:
-                print("===========================================")
-                if comCodigo and "_id" in vendedor:
-                    print(f'Id: {vendedor["_id"]}') 
-                vendedorJson(vendedor)
-            print("===========================================")
-            return True
-    else:
-        vendedor = vendedores.find_one({"_id": ObjectId(dadoProcurado)})
-        if vendedor:
-            print("Vendedor encontrado!")
-            return vendedor
-        else:
-            print("Nenhum Vendedor encontrado!")
-            return None
+            vendedorJson(vendedor, comCodigo)
+        print("===========================================")
+        return listaVendedores if len(listaVendedores) > 1 else listaVendedores[0]
+    return buscarPorId(vendedores, dadoProcurado)
 
-def buscarProduto(dadoProcurado, tipoDado, comCodigo, comVendedor):
+def buscarProduto(dadoProcurado, tipoDado, comCodigo=False, comVendedor=False):
     produtos = conectar().Produto
     if tipoDado == 'nome':
-        if dadoProcurado == '':
-            listaProdutos = produtos.find().sort("nome_produto")
-        else:
-            listaProdutos = produtos.find({"nome_produto": dadoProcurado}).sort("nome_produto")
-        count = 0
-        listaProdutos = [*listaProdutos]
-        if len(listaProdutos) == 0:
+        filtro = {} if dadoProcurado == '' else {"nome_produto": dadoProcurado}
+        listaProdutos = buscarDocumento(produtos, filtro, "nome_produto")
+        if not listaProdutos:
             print("Nenhum produto encontrado!")
             return False
-        elif len(listaProdutos) == 1:
-            produto = listaProdutos[0]
+        for produto in listaProdutos:
             print("===========================================")
-            if comCodigo and "_id" in produto:
-                print(f'Id: {produto["_id"]}') 
-            produtoJson(produto, comVendedor)
-            print("===========================================")
-            return produto
-        else:
-            for produto in listaProdutos:
-                print("===========================================")
-                if comCodigo and "_id" in produto:
-                    print(f'Id: {produto["_id"]}') 
-                produtoJson(produto, comVendedor)
-            print("===========================================")
-            return True
-    else:
-        produto = produtos.find_one({"_id": ObjectId(dadoProcurado)})
-        if produto:
-            print("Produto encontrado!")
-            return produto
-        else:
-            print("Nenhum produto encontrado!")
-            return None
+            produtoJson(produto, comCodigo=comCodigo, comVendedor=comVendedor)
+        print("===========================================")
+        return listaProdutos if len(listaProdutos) > 1 else listaProdutos[0]
+    return buscarPorId(produtos, dadoProcurado)
