834bc14028507e3afc9eea4475f211560dbeb06e
BrunoSerpa <serpabruno2005@gmail.com>
2024-06-21 10:19:21 -0300

    Arrumei formatações e suas máscaras

--- Sistema/formatacaoJson.py
+++ Sistema/formatacaoJson.py
@@ -4,7 +4,7 @@ def enderecoJson(arquivoJson):
     if "cidade" in arquivoJson and "estado" in arquivoJson and "pais" in arquivoJson:
         print(f'{arquivoJson["cidade"]} - {arquivoJson["estado"]} ({arquivoJson["pais"]})')
     if "cep" in arquivoJson:
-        print(f'CEP: {arquivoJson["cep"]}')
+        print(f'CEP: {formatar_cep(arquivoJson["cep"])}')
 
 def produtoJson(arquivoJson, comCodigo=False, comVendedor=False):
     if comCodigo and "_id" in arquivoJson: print(f'Id: {arquivoJson["_id"]}')
@@ -13,12 +13,12 @@ def produtoJson(arquivoJson, comCodigo=False, comVendedor=False):
     if comVendedor and "nome_vendedor" in arquivoJson: print(f'Vendedor: {arquivoJson["nome_vendedor"]}')
     if comVendedor and "cnpj" in arquivoJson: print(f'CNPJ: {arquivoJson["cnpj"]}')
 
-def vendedorJson(arquivoJson):
+def vendedorJson(arquivoJson, comCodigo=False):
+    if comCodigo and "_id" in arquivoJson: print(f'Id: {arquivoJson["_id"]}')
     if "nome_vendedor" in arquivoJson: print(f'Nome: {arquivoJson["nome_vendedor"]}')
-    if "cnpj" in arquivoJson:
-        print(f'CNPJ: {arquivoJson["cnpj"]}')
+    if "cnpj" in arquivoJson: print(f'CNPJ: {formatar_cnpj(arquivoJson["cnpj"])}')
     if "email_vendedor" in arquivoJson: print(f'Email: {arquivoJson["email_vendedor"]}')
-    if "telefone_vendedor" in arquivoJson: print(f'Telefone: {arquivoJson["telefone_vendedor"]}')
+    if "telefone_vendedor" in arquivoJson: print(f'Telefone: {formatar_telefone(arquivoJson["telefone_vendedor"])}')
     if "enderecos" in arquivoJson:
         count = 0
         for endereco in arquivoJson["enderecos"]:
@@ -46,9 +46,9 @@ def vendedorJson(arquivoJson):
 def usuarioJson(arquivoJson, comCodigo=False):
     if comCodigo and "_id" in arquivoJson: print(f'Id: {arquivoJson["_id"]}')
     if "nome_usuario" in arquivoJson: print(f'Nome: {arquivoJson["nome_usuario"]}')
-    if "cpf" in arquivoJson: print(f'CPF: {arquivoJson["cpf"]}')
+    if "cpf" in arquivoJson: print(f'CPF: {formatar_cpf(arquivoJson["cpf"])}')
     if "email_usuario" in arquivoJson: print(f'Email: {arquivoJson["email_usuario"]}')
-    if "telefone_usuario" in arquivoJson: print(f'Telefone: {arquivoJson["telefone_usuario"]}')
+    if "telefone_usuario" in arquivoJson: print(f'Telefone: {formatar_telefone(arquivoJson["telefone_usuario"])}')
     if "enderecos" in arquivoJson:
         count = 0
         for endereco in arquivoJson["enderecos"]:
@@ -62,7 +62,7 @@ def usuarioJson(arquivoJson, comCodigo=False):
             count1 += 1
             print("-----------------------------------------------")
             print(f'{count1}º Produto Favorito:')
-            produtoJson(produto, True)
+            produtoJson(produto, False)
     if "compras" in arquivoJson:
         count2 = 0
         for compra in arquivoJson["compras"]:
@@ -100,6 +100,8 @@ def comprasJson(arquivoJson, comCliente, comVendedor):
     if "valor_total" in arquivoJson: print(f'Valor total: R${arquivoJson["valor_total"]:.2f}')
     
 def formatar_telefone(telefone):
+    if len(telefone) == 8: return f'({telefone[0:4]}-{telefone[4:8]}'
+    if len(telefone) == 9: return f'({telefone[0:5]}-{telefone[5:9]}'
     if len(telefone) == 10: return f'({telefone[0:2]}) {telefone[2:6]}-{telefone[6:10]}'
     if len(telefone) == 11: return f'({telefone[0:2]}) {telefone[2:7]}-{telefone[7:11]}'
     else: return telefone
@@ -107,8 +109,11 @@ def formatar_telefone(telefone):
 def formatar_cpf(cpf):
     if len(cpf) == 11: return f'{cpf[0:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:11]}'
     else: return cpf
-    
+
 def formatar_cnpj(cnpj):
     if len(cnpj) == 14: return f'{cnpj[0:2]}.{cnpj[2:5]}.{cnpj[5:8]}/{cnpj[8:12]}-{cnpj[12:14]}'
     else: return cnpj
-
+    
+def formatar_cep(cep):
+    if len(cep) == 8: return f'{cep[0:5]}-{cep[5:9]}'
+    else: return cep
