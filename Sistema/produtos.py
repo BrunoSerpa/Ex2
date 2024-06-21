cb37c4607211ac3a56e66c883fdca2212900aa40
BrunoSerpa <serpabruno2005@gmail.com>
2024-06-21 13:14:19 -0300

    Melhorei as funções de produtos

--- Sistema/produtos.py
+++ Sistema/produtos.py
@@ -1,58 +1,48 @@
 from pymongo.mongo_client import MongoClient
-from bson.objectid import ObjectId
 import os
-# CONEXÃO COM A COLEÇÃO DE PRODUTO
+from bson.objectid import ObjectId
 from conexaoMongo import conectar
-global produtos
-produtos = conectar().Produto
 from validacoes import validarNaoVazio, validarFloat
-from busca import buscarProduto, buscaVendedor
 from formatacaoJson import produtoJson
+from busca import buscarProduto, buscarVendedor
 
-def atualizandoProdutosVendedor(produtos, idVendedor):
-    vendedores= conectar().Vendedor
+produtos = conectar().Produto
+
+def atualizarProdutos(produtos, idVendedor):
+    vendedores = conectar().Vendedor
     try:
-        atualizou = vendedores.update_one({"_id": ObjectId(idVendedor)}, {"$set": {"produtos": produtos}})
-        print("Produto atualizados com sucesso")
+        vendedores.update_one({"_id": ObjectId(idVendedor)}, {"$set": {"produtos": produtos}})
+        print("Produtos atualizados com sucesso")
         return True
     except Exception as e:
         print(f"Erro ao vincular o produto: {e}")
+        input("Insira qualquer coisa para continuar...")
     return False
 
-def cadastrarProduto(vendedor):
+def obterEntrada(mensagem, validacao, erroMensagem):
+    while True:
+        entrada = input(mensagem)
+        if not validacao(entrada):
+            print(erroMensagem)
+        else:
+            return entrada
+
+def cadastrarProduto(vendedor=None):
     os.system('cls')
     print("Cadastrando produto...")
-    arrumaVendedor = False
-    if not vendedor:
-        while True:
-            achouVendedor = buscaVendedor(input('Insira o nome do vendedor: '), 'nome', True)
-            if not achouVendedor:
-                if input("Deseja procurar novamente? (S/N)\n").upper() == 'S': continue
-                return
-            elif isinstance(achouVendedor, dict):
-                vendedor = achouVendedor 
-            elif not isinstance(achouVendedor, dict):
-                while True:
-                    vendedor = buscaVendedor(input('Insira o id do vendedor desejado: '), 'id', False)
-                    if vendedor == None:
-                        if input("Deseja procurar novamente? (S/N)\n").upper() == 'S': continue
-                        else: return
-                    break
-            arrumaVendedor = True
-            break
-    while True:
-        nomeProduto=input("Insira o nome do produto: ")
-        if not validarNaoVazio(nomeProduto): 
-            print("Nome não pode estar em branco.")
-            continue
-        break
-    while True:
-        valorProduto = input("Insira o valor do produto: R$")
-        if not validarFloat(valorProduto): 
-            print("O valor só pode ser um número.")
-            continue
-        valorProduto = float(valorProduto)
-        break
+    atualizaVendedor = False
+    while not vendedor:
+        atualizaVendedor = True
+        achouVendedor = buscarVendedor(input('Insira o nome do vendedor desejado: '), 'nome', True)
+        if isinstance(achouVendedor, dict): vendedor = achouVendedor
+        elif not achouVendedor:
+            if input("Deseja procurar novamente? (S/N)\n").upper() != 'S': return
+        else:
+            vendedor = buscarVendedor(input('Insira o id do vendedor desejado: '), 'id', False)
+            if vendedor == None:
+                if input("Deseja procurar novamente? (S/N)\n").upper() != 'S': return
+    nomeProduto = obterEntrada("Insira o nome do produto: ", validarNaoVazio, "Nome não pode estar em branco.")
+    valorProduto = float(obterEntrada("Insira o valor do produto: R$", validarFloat, "O valor só pode ser um número."))
     produto = {
         "id_vendedor": vendedor["_id"],
         "nome_vendedor": vendedor["nome_vendedor"],
@@ -62,155 +52,130 @@ def cadastrarProduto(vendedor):
     try:
         produtos.insert_one(produto)
         print("Produto cadastrado com sucesso!")
-        if arrumaVendedor:
-            vendedor["produtos"] = [*vendedor["produtos"]]
-            vendedor["produtos"].append(produto)
-            if not atualizandoProdutosVendedor(vendedor["produtos"], vendedor["_id"]):
-                produtos.delete_one({ "_id": produto["_id"] })
+        vendedor["produtos"].append(produto)
+        if atualizaVendedor:
+            atualizarProdutos(vendedor["produtos"], vendedor["_id"])
     except Exception as e:
         print(f"Erro ao cadastrar produto: {e}")
+        input("Insira qualquer coisa para continuar...")
     return produto
 
-# FUNÇÃO LISTAR
 def listarProduto():
     os.system('cls')
     print("Listando produtos...")
     if input("Deseja procurar um produto específico? (S/N)\n").upper() == 'S':
         while True:
             nomeProduto = input('Insira o nome do produto desejado: ')
-            achouProduto = buscarProduto(nomeProduto, 'nome', False)
+            achouProduto = buscarProduto(nomeProduto, 'nome', False, True)
             if not achouProduto:
                 if input("Deseja procurar novamente? (S/N)\n").upper() == 'S': continue
             break
     else:
         print('Produtos Existentes:')
-        listaProdutos = produtos.find().sort("nome_produto")
-        count = 0
-        for produto in listaProdutos:
-            count += 1
-            print("===========================================")
-            produtoJson(produto)
-        if count == 0:
-            print("Nenhum produto cadastrado!")
-        else:
-            print("===========================================")
+        buscarProduto('', 'nome', False, True)
 
-def deletarProduto(produto):
-    arrumaVendedor = False
-    if not produto:
-        while True:
-            achouProduto = buscarProduto(input('Insira o nome do produto desejado: '), 'nome', True, True)
-            if not achouProduto:
-                if input("Deseja procurar novamente? (S/N)\n").upper() == 'S': continue
-                return
-            elif isinstance(achouProduto, dict):
-                produto = achouProduto 
-            elif not isinstance(produto, dict):
-                while True:
-                    produto = buscarProduto(input('Insira o id do produto desejado: '), 'id', False, True)
-                    if produto == None:
-                        if input("Deseja procurar novamente? (S/N)\n").upper() == 'S': continue
-                        else: return
-                    break
-            break
-        arrumaVendedor = True
+def atualizarProduto(produto=None):
+    atualizaVendedor = False
+    while not produto:
+        atualizaVendedor = True
+        os.system('cls')
+        print("Editando produto...")
+        achouProduto = buscarProduto(input('Insira o nome do produto desejado: '), 'nome', True, True)
+        if isinstance(achouProduto, dict): produto = achouProduto
+        elif not achouProduto:
+            if input("Deseja procurar novamente? (S/N)\n").upper() != 'S': return
+        else:
+            produto = buscarProduto(input('Insira o id do produto desejado: '), 'id', False)
+            if not produto:
+                if input("Deseja procurar novamente? (S/N)\n").upper() != 'S': return
+    produto["nome_produto"] = obterEntrada("Insira o novo nome do produto: ", validarNaoVazio, "Nome não pode estar em branco.")
+    produto["valor_produto"] = float(obterEntrada("Insira o novo valor do produto: R$", validarFloat, "O valor só pode ser um número."))
     try:
-        produtos.delete_one({ "_id": produto["_id"] })
-        print("Produto deletado com sucesso!!")
-        if arrumaVendedor:
-            vendedor = buscaVendedor(produto["id_vendedor"], 'id', False)
-            listaProdutos = [*vendedor["produtos"]]
-            listaProdutos.remove(produto)
-            if not atualizandoProdutosVendedor(listaProdutos, produto["id_vendedor"]):
-                produtos.insert_one(produto)
-        return True
+        produtos.update_one({"_id": produto["_id"]}, {"$set": produto})
+    except Exception as e:
+        print(f"Erro ao atualizar o produto: {e}")
+        input("Insira qualquer coisa para continuar...")
+    if atualizaVendedor:
+        vendedor = buscarVendedor(produto["id_vendedor"], 'id', False)
+        if vendedor:
+            produtos_vendedor = [produto if p["_id"] == produto["_id"] else p for p in vendedor["produtos"]]
+            atualizarProdutos(produtos_vendedor, vendedor["_id"])
+        print("Produto e vendedor atualizados com sucesso!")
+    else: print("Produto atualizado com sucesso!")
+    return produto
+
+def deletarProduto(produto=None):
+    atualizaVendedor = False
+    while not produto:
+        atualizaVendedor = True
+        os.system('cls')
+        print("Editando produto...")
+        achouProduto = buscarProduto(input('Insira o nome do produto desejado: '), 'nome', True, True)
+        if isinstance(achouProduto, dict): produto = achouProduto
+        elif not achouProduto:
+            if input("Deseja procurar novamente? (S/N)\n").upper() != 'S': return
+        else:
+            produto = buscarProduto(input('Insira o id do produto desejado: '), 'id', False)
+            if not produto:
+                if input("Deseja procurar novamente? (S/N)\n").upper() != 'S': return
+    try:
+        produtos.delete_one({"_id": produto["_id"]})
     except Exception as e:
         print(f"Erro ao deletar o produto: {e}")
-        return False
+        input("Insira qualquer coisa para continuar...")
+    if atualizaVendedor:
+        vendedor = buscarVendedor(produto["id_vendedor"], 'id', False)
+        if vendedor:
+            vendedor["produtos"] = [p for p in vendedor["produtos"] if p["_id"] != produto["_id"]]
+            atualizarProdutos(vendedor["produtos"], vendedor["_id"])
+        print("Produto deletado com sucesso! Produtos do vendedor atualizados!")
+    else:
+        print("Produto deletado com sucesso!")
 
 def gerenciarProdutos(produtos, idVendedor):
-    ids=[]
+    ids = []
+    os.system('cls')
     print("Produtos Atuais:")
-    if not produtos: 
+    if not produtos:
         print("Nenhum produto cadastrado!")
     else:
         for produto in produtos:
-            print("================================")
-            print(f'Código Produto: {produto["_id"]}')
             ids.append(produto["_id"])
-            produtoJson(produto)
+            print("================================")
+            produtoJson(produto, True, False)
         print("================================")
-    print('O que deseja fazer com os produtos?')
-    print('1 - Criar um novo Produto')
-    if produtos:
-        print('2 - Editar um produto existente')
-        print('3 - Deletar um produto existente')
-    print('0 - Voltar')
-    opcao = int(input('Insira a opção desejada: '))
-    if opcao == 0:
-        return produtos
-    elif opcao == 1:
-        novoproduto = cadastrarProduto(idVendedor)
-        if novoproduto:
-            produtos.append(novoproduto)
-        return produtos
-    elif produtos:  
-        if opcao == 2:
-            idProduto = ObjectId(input("Insira o id do objeto:"))
-            if idProduto in ids: 
-                for produto in produtos:
-                    if produto["_id"] == idProduto:
-                        produto = atualizarProduto(produto)
-            return produtos
-        elif opcao == 3:
-            idProduto = ObjectId(input("Insira o id do objeto:"))
-            if idProduto in ids: 
-                for produto in produtos:
-                    if produto["_id"] == idProduto:
-                        deletou = deletarProduto(produto)
-                        if deletou: produtos.remove(produto)
-            return produtos
-    print('Comando incorreto! :(')
-    return produtos
+    print("")
+    print("================================")
+    print("O que deseja fazer com os produtos?")
+    print("--------------------------------")
+    print("1 - Criar um novo Produto")
+    if len(ids) > 0:
+        print("2 - Editar um produto existente")
+        print("3 - Deletar um produto existente")
+    print("--------------------------------")
+    print("0 - Voltar")
+    print("================================")
+    opcao = input("Insira uma opção: ")
+    if opcao == '1':
+        vendedor = buscarVendedor(idVendedor, 'id', False)
+        if vendedor:
+            novoProduto = cadastrarProduto(vendedor)
+            if novoProduto: produtos.append(novoProduto)
+    elif opcao in ['2', '3'] and len(ids) > 0:
+        idProduto = input("Insira o id do produto desejado: ")
+        try:
+            idProduto = ObjectId(idProduto)
+            produto = next((p for p in produtos if p["_id"] == idProduto), None)
+            if not produto:
+                print("ID inválido.")
+                input("Insira qualquer coisa para continuar...")
+            elif opcao == '2':
+                produtoAtualizado = atualizarProduto(produto)
+                produtos = [produtoAtualizado if p["_id"] == produtoAtualizado["_id"] else p for p in produtos]
+            elif opcao == '3':
+                if deletarProduto(produto): produtos.remove(produto)
+        except Exception as e:
+            print(f"Erro ao processar o ID: {e}")
 
-def atualizarProduto(produto):
-    arrumarVendedor = False
-    while True:
-        if not produto:
-            achouProduto = buscarProduto(input('Insira o nome do produto desejado: '), 'nome', True)
-            if not achouProduto:
-                if input("Deseja procurar novamente? (S/N)\n").upper() == 'S': 
-                    continue
-                else: 
-                    return
-            produto = buscaVendedor(input('Insira o id do produto desejado: '), 'id', False)
-            if produto == None:
-                if input("Deseja procurar novamente? (S/N)\n").upper() == 'S': 
-                    continue
-                else: 
-                    return
-            arrumarVendedor = True
-            break
-    while True:
-        nomeProduto=input("Insira o nome do produto: ")
-        if not validarNaoVazio(nomeProduto): 
-            print("Nome não pode estar em branco.")
-            continue
-        break
-    while True:
-        valorProduto = input("Insira o valor do produto: R$")
-        if not validarFloat(valorProduto): 
-            print("O valor só pode ser um número.")
-            continue
-        valorProduto = float(valorProduto)
-        break
-    produto["nome_produto"]=nomeProduto
-    produto["valor_produto"]=valorProduto
-    try:
-        produtos.update_one({ "_id": produto["_id"] },{ "$set": produto })
-        print("Produto atualizado com sucesso!!")
-        if arrumarVendedor: 
-            atualizarProduto(produto, "editar")
-    except Exception as e:
-        print(f"Erro ao atualizar o produto: {e}")
-    return produto
+    elif opcao != "0": print('Comando inválido.')
+    return produtos
