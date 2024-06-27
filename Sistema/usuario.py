85b70411d6c04777fe8868b5e8261f1f8f17d6b1
BrunoSerpa <serpabruno2005@gmail.com>
2024-06-26 21:41:16 -0300

    Inseri o redis as funções do usuário

--- Sistema/usuario.py
+++ Sistema/usuario.py
@@ -1,6 +1,7 @@
 from pymongo.mongo_client import MongoClient
 import os
 from conexaoMongo import conectar
+from conexaoRedis import cacheResultado, obterCache
 from validacoes import validarNaoVazio, validarCPF, validarEmail, validarTelefone
 from formatacaoJson import usuarioJson, enderecoJson, produtoJson
 from busca import buscarUsuario
@@ -35,23 +36,18 @@ def cadastrarUsuario():
     try:
         usuarios.insert_one(novoUsuario)
         print("Usuário cadastrado com sucesso!")
+        cacheResultado(f'usuario:id:{novoUsuario["_id"]}', novoUsuario)
     except Exception as e:
         print(f"Erro ao cadastrar usuário: {e}")
         input("Insira qualquer coisa para continuar...")
 
-def atualizarUsuario():
+def atualizarUsuario(usuario = None):
     os.system('cls')
     print("Editando usuário...")
-    usuario = None
-    while not usuario:
-        achouUsuario = buscarUsuario(input('Insira o nome do usuário desejado: '), 'nome', True)
-        if not achouUsuario:
-            if input("Deseja procurar novamente? (S/N)\n").upper() != 'S': return
-        elif not isinstance(achouUsuario, dict):
-            usuario = buscarUsuario(input('Insira o id do usuário desejado: '), 'id', False)
-            if not usuario:
-                if input("Deseja procurar novamente? (S/N)\n").upper() != 'S': return
-        else: usuario = achouUsuario
+    if not usuario:
+        print("Nenhum usuário foi encontrado!")
+        return
+    chaveCache = f"usuario:id:{usuario.get('_id')}"
     while True:
         usuarioJson(usuario)
         print("================================")
@@ -74,39 +70,37 @@ def atualizarUsuario():
         if opcao == "0":
             try:
                 usuarios.update_one({"_id": usuario["_id"]}, {"$set": usuario})
+                cacheResultado(chaveCache, usuario)
                 print("Usuário atualizado com sucesso!")
             except Exception as e:
                 print(f"Erro ao editar usuário: {e}")
                 input("Insira qualquer coisa para continuar...")
             break
-        elif opcao == "1":
-            usuario["nome_usuario"] = obterEntrada("Insira o novo nome do usuário: ", validarNaoVazio, "Nome não pode estar em branco.")
-        elif opcao == "2":
-            usuario["cpf"] = obterEntrada("Insira o novo CPF do usuário: ", validarCPF, "CPF inválido. Deve conter 11 dígitos numéricos.")
-        elif opcao == "3":
-            usuario["email_usuario"] = obterEntrada("Insira o email do usuário: ", validarEmail, "Email inválido. Certifique-se de que contém '@' e '.'.")
-        elif opcao == "4":
-            usuario["telefone_usuario"] = obterEntrada("Insira o telefone do usuário: ", validarTelefone, "Telefone inválido. Deve conter apenas dígitos numéricos e ter pelo menos 8 caracteres.")
-        elif opcao == "5":
-            usuario["enderecos"] = gerenciarEnderecos(usuario.get("enderecos", []))
-        elif opcao == "6":
-            usuario["favoritos"] = gerenciarFavoritos(usuario.get("favoritos", []))
+        elif opcao == "1": usuario["nome_usuario"] = obterEntrada("Insira o novo nome do usuário: ", validarNaoVazio, "Nome não pode estar em branco.")
+        elif opcao == "2": usuario["cpf"] = obterEntrada("Insira o novo CPF do usuário: ", validarCPF, "CPF inválido. Deve conter 11 dígitos numéricos.")
+        elif opcao == "3": usuario["email_usuario"] = obterEntrada("Insira o email do usuário: ", validarEmail, "Email inválido. Certifique-se de que contém '@' e '.'.")
+        elif opcao == "4": usuario["telefone_usuario"] = obterEntrada("Insira o telefone do usuário: ", validarTelefone, "Telefone inválido. Deve conter apenas dígitos numéricos e ter pelo menos 8 caracteres.")
+        elif opcao == "5": usuario["enderecos"] = gerenciarEnderecos(usuario.get("enderecos", []))
+        elif opcao == "6": usuario["favoritos"] = gerenciarFavoritos(usuario.get("favoritos", []))
         elif opcao == "7":
             novaCompra = fazerCompra(usuario)
             if novaCompra:
-               if "compras" in usuario:
-                   usuario["compras"].append(novaCompra)
-               else:
-                   usuario["compras"]=[novaCompra]
+                if "compras" in usuario: usuario["compras"].append(novaCompra)
+                else: usuario["compras"] = [novaCompra]
+
 def deletarUsuario():
     while True:
-        achouUsuario = buscarUsuario(input('Insira o nome do usuário desejado: '), 'nome', True)
-        if not achouUsuario:
-            if input("Deseja procurar novamente? (S/N)\n").upper() == 'S': continue    
+        dadoProcurado = input('Insira o nome do usuário desejado: ')
+        chaveCache = f"usuario:nome:{dadoProcurado}"
+        usuario = obterCache(chaveCache)
+        if not usuario:
+            achouUsuario = buscarUsuario(dadoProcurado, 'nome', True)
+            if not achouUsuario:
+                if input("Deseja procurar novamente? (S/N)\n").upper() == 'S': continue
             usuario = buscarUsuario(input('Insira o id do usuário desejado: '), 'id', False)
             if not usuario:
                 if input("Deseja procurar novamente? (S/N)\n").upper() != 'S': return
-        else: usuario = achouUsuario
+            cacheResultado(chaveCache, usuario)
         break
     try:
         usuarios.delete_one(usuario)
